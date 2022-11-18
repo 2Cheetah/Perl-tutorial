14a80f2919eb8e3a2bc96c1befd4f30d5001c4ee
2Cheetah <fckninja@gmail.com>
2022-11-18 14:11:12 +0800

    The double diamond and several more sections were added
    notes_chapter5.md and sandbox_chapter5.pl files were updated, sandbox_ch5_print.pl file was added

---
+++ exercises/sandbox_ch5_print.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use warnings;
+
+@lines = <STDIN>;
+print @lines;
+print "\n";
+print "@lines";
+print "\n";
+
--- exercises/sandbox_chapter5.pl
+++ exercises/sandbox_chapter5.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use warnings;
+use Getopt::Long;
 
 #while (defined($line = <STDIN>)) {
 #    print "I saw $line";
@@ -9,7 +10,16 @@ use warnings;
 #    print "I saw $_";
 #}
 
-while (<>) {
-    chomp;
-    print "It was \"$_\" that I saw.\n";
-}
+#while (<>) {
+#    chomp;
+#    print "It was \"$_\" that I saw.\n";
+#}
+
+my $verbose = '';
+my $all = '';
+my $tag = '';
+
+GetOptions ('verbose!' => \$verbose, 'all!' => \$all, 'tag=s' => \$tag);
+print "Verbose option is $verbose\n";
+print "All option is $all\n";
+print "Tag is $tag\n";
