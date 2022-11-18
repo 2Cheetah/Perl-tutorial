2e03d7014bb8e232438fb0497df9cbe65fdee2dd
2Cheetah <fckninja@gmail.com>
2022-11-18 19:33:26 +0800

    Bad Filehandles section and several more were added
    notes_chapter5.md file was updated

--- .vscode/perl-lang/home/denis/perl/exercises/calculus.pl
+++
@@ -1,1 +0,0 @@
-{"version":5,"vars":[{"name":"$what","definition":"my","containerName":null,"kind":13,"line":20,"localvar":"my"},{"line":21,"kind":13,"name":"$n","definition":"my","containerName":null,"localvar":"my"}]}
